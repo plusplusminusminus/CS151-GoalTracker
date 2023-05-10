f41cb2afc2779557e066a2fbbb53e8ebd3373ffc
Tyler Moquin <124649324+plusplusminusminus@users.noreply.github.com>
2023-05-09 22:29:31 -0700

    Add files via upload

---
+++ GoalTracker/src/main/java/com/goaltracker/forms/pages/Page.java
@@ -0,0 +1,22 @@
+/*
+ * Page.java
+ * - flag interface for all pages
+ *
+ * Tyler Moquin
+ * SJSU Spring 2023
+ * CS 151, Sec 6
+ * Instructor Gaikwad
+ *
+ * Final Project, "Group 11"
+ *
+ * VERSION HISTORY
+ * v1.0.0   20230509   TAM   final working version
+ * v0.0.1   20230321   TAM   initial implementation
+ *
+ */
+
+package com.goaltracker.forms.pages;
+
+public interface Page {
+
+}
